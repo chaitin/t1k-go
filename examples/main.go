f178fc2911ed13601f3501aaf7b3f5ef9abf1cce
xiaobing.wang <xiaobing.wang@chaitin.com>
2024-07-10 17:12:00 +0800

    feat: add pool

--- examples/main.go
+++ examples/main.go
@@ -10,15 +10,17 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		d := t1k.NewDetector(t1k.Config{
-			Addr: os.Getenv("DETECTOR_ADDR"),
-		})
-		res, err := d.DetectorRequest(r)
+		server, err := t1k.NewWithPoolSize(os.Getenv("DETECTOR_ADDR"), 10)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		if !res.Allowed() {
+		res, err := server.DetectHttpRequest(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if !res.Blocked() {
 			http.Error(w, fmt.Sprintf("blocked event id %s", res.EventID()), res.StatusCode())
 			return
 		}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xbingW/t1k
 
 go 1.17
+
+require golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028
